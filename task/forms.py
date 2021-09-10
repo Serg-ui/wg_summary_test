8b7e60ac6b6e62ca67cf44581e54930d7f7192a2
Serg <serrg.y85@gmail.com>
2021-09-10 15:13:05 +0300

    add bootstarp

--- task/forms.py
+++ task/forms.py
@@ -2,6 +2,15 @@ from django import forms
 
 
 class UploadFileForm(forms.Form):
-    file = forms.FileField(label='Основной файл')
+    file = forms.FileField(label='Основной файл',
+                           widget=forms.ClearableFileInput(attrs={
+                               'class': 'form-control',
+                               'id': 'inputGroupFile01'
+                           })
+                           )
     files = forms.FileField(label='Файлы для сравнения с основным',
-                            widget=forms.ClearableFileInput(attrs={'multiple': True}))
+                            widget=forms.ClearableFileInput(attrs={
+                                'multiple': True,
+                                'class': 'form-control',
+                                'id': 'inputGroupFile02'
+                            }))
